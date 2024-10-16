b9a5e359e5ec700dca86f853d003a3dec3a36318
Dhilaharis <126457555+Dhilaharis@users.noreply.github.com>
2024-10-16 11:18:39 +0530

    Add files via upload

---
+++ appemail/views.py
@@ -0,0 +1,28 @@
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.files.storage import FileSystemStorage
+
+def send_email_with_attachment(username, recipient_email, subject, message, file=None):
+    # Create email subject and body using context
+    email_subject = subject
+    email_body = render_to_string('email_template.html', {
+        'username': username,
+        'message': message,
+    })
+    
+    # Create the email
+    email = EmailMessage(
+        email_subject,
+        email_body,
+        settings.DEFAULT_FROM_EMAIL,
+        [recipient_email],
+    )
+    
+    # Attach file if provided
+    if file:
+        email.attach(file.name, file.read(), file.content_type)
+    
+    # Send email
+    email.send()
+
